2ecdb7a462573a9255a9179763e024fe6e1113fe
Mojoxz <150643006+Mojoxz@users.noreply.github.com>
2024-11-07 09:41:58 +0700

    Update firebase_option.dart

--- lib/firebase_option.dart
+++ lib/firebase_option.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 
 FirebaseOptions firebaseOptions = Platform.isAndroid
     ? const FirebaseOptions(
-        apiKey: 'AIzaSyCLtjPL3AW33AZnbzDMQZkaOMjvfLYym08',
+        apiKey: '',
         appId: '1:892913698510:android:d729e25ed990f73e8fb29f',
         messagingSenderId: '892913698510',
         projectId: 'sepatuflutter')
     : const FirebaseOptions(
-        apiKey: 'AIzaSyBfoVDkpxEw30eX0SPWZPWLrvfFSq0c2Ps',
+        apiKey: '',
         appId: '1:892913698510:ios:46f83a481ddb95c08fb29f',
         messagingSenderId: '892913698510',
         projectId: 'sepatuflutter');
